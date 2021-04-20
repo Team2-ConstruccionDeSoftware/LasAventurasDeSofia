fb69ccefec449d0333ee0c6c02cfc7954f865ef6
didimelor <A01023785@itesm.mx>
2021-04-20 09:15:10 -0500

    Se puede morir, implementé las vidas, efectos de sonido y empece con la escena del level select

---
+++ Assets/scripts/GameManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public void endGame(){
+        //FindObjectOfType<GameManager>().endGame();
+        if(gameHasEnded == false){
+            Debug.Log("entramos");
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("restart", restartDelay);
+        }
+    }
+    void restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
