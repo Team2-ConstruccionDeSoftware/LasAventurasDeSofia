84714d75c963979881c45575271d6255c6caadf7
Manzanis <a01029111@itesm.mx>
2021-04-21 13:31:09 -0500

    Layout del nivel 2 terminado, agregado plataformas ascendentes. Falta musica, enemigos y preguntas

---
+++ Assets/scripts/Create_Platforms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create_Platforms : MonoBehaviour
+{
+    [SerializeField] GameObject Platform;
+    [SerializeField] Vector2 limits;
+    [SerializeField] float delay;
+    
+    public float numberPlats = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("NewPlat", 1, delay);
+    }
+
+    // Update is called once per frame
+    void NewPlat()
+    {
+        Instantiate(Platform, transform.position, Quaternion.identity);
+    }
+}
