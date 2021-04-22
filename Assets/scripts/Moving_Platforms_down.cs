d184558b1f8e466efec053c03f5a2b614d2d0776
Manzanis <a01029111@itesm.mx>
2021-04-22 10:03:14 -0500

    Layout del Nivel 3 terminado, agregado plataformas movibles. Faltan enemigos, preguntas y final

---
+++ Assets/scripts/Moving_Platforms_down.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_Platforms_down : MonoBehaviour
+{
+    [SerializeField] float vel;
+    [SerializeField] float Limit;
+    private Rigidbody2D rd;
+    private SpriteRenderer sr;
+    
+
+    private void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+        rd.velocity = new Vector2(0f, vel);
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //rd.velocity = new Vector2(vel, 0f);
+       
+        if(transform.position.y < Limit)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
